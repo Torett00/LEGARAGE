ef46ddcec9c2427c7678331e2bb39899a3f42134
khaled charbti <charbtikhaled001@gmail.com>
2025-03-13 21:50:15 +0100

    Links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  navigateToFacebock(): void {
+   
+    // 
+    //  // Redirect to 'target' route
+
+   
+      window.open('https://www.https://www.facebook.com/profile.php?id=100086913193624.com/profile.php?id=100063595867837', '_blank');
+  
+  }
+  navigateToInsta(): void {
+   
+    //   // Redirect to 'target' route
+    window.open('https://www.instagram.com/https://www.instagram.com/cafe_le_garage//', '_blank');
+  }
 }
